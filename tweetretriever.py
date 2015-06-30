f0f0ad8f1acb99f70fcf54f7742d20659123f379
Evgeny <evgeny.she+git@gmail.com>
2015-06-30 14:27:49 +0300

    Update tweetretriever.py
    Few major cleanups. Not tested, though

--- tweetretriever.py
+++ tweetretriever.py
@@ -1,4 +1,6 @@
 #import urlopen from the url library, and logging
+# avoid useless comments
+
 from urllib import urlopen
 import logging
 
@@ -8,15 +10,17 @@ logger = logging.getLogger(__name__)
 def getOnlineTweets(url):
 
 	#raw list of tweets from the webpage specified
-	rawlist = urlopen(url).read()
+	# rawlist = urlopen(url).read()
 
 	#splitting each line (tweet) into the list tweetlist
-	tweetlist = rawlist.splitlines()
+	# tweetlist = rawlist.splitlines()
 	
-	return tweetlist
+	return urlopen(url).readlines()		# should work
+	# return is a last operation in functions. all other will be ingored, so too late for logging
 	logger.debug("Online tweets retrieved successfully.")
 
 def getPastTweets():
+	# same as getOnlineTweets
 	pastTweets = open("Tweets/pasttweets.txt", "r")
 	
 	rawlist = pastTweets.read()
@@ -25,25 +29,28 @@ def getPastTweets():
 
 	return tweetlist
 	logger.debug("Past tweets retrieved successfully.")
-
+	# too late for closing file
 	pastTweets.close()
 	
 def newTweets(onlineTweets, pastTweets):
 		
-	addTweet = True
-	newTweets = []
+	# addTweet = True
+	# newTweets = []
 		
-	for x in onlineTweets:
-		for y in pastTweets:
-			if x == y:
-				addTweet = False
+	# for x in onlineTweets:
+	# 	for y in pastTweets:
+	# 		if x == y:
+	# 			addTweet = False
 			
-		if addTweet == True:
-			newTweets.append(x)
-		else:
-			addTweet = True
+	# 	if addTweet == True:
+	# 		newTweets.append(x)
+	# 	else:
+	# 		addTweet = True
 			
-	return newTweets
+	# return newTweets
+
+	return [tweet for tweet in onlineTweets if tweet not in pastTweets]
+	# also this is even easier (and faster) with sets instead of lists.
 	logger.debug("New tweets successfully compared, results are: \n%s" % (newTweets))
 	
 def writeToPastTweets(statusupdate):
